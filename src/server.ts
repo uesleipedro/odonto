1213ca2d33a9fe1f79507491a3a49d6a619b08e0
uesleipedro <ielseu@hotmail.com>
2023-08-22 11:21:09 -0300

    estrutura de diretorios

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+//import 'dotenv/config';
+import { app } from './app';
+
+const port = (process.env.PORT || '3000');
+
+const server =
+    app.listen(port, () => console.log(`running on port ${port}`));
+
+process.on('SIGINT', () => {
+    server.close();
+    console.log('Connection closed');
+});
