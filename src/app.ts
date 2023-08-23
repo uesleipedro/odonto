f8bde122bc33fa2c897700c01d56b0f13125f5b2
uesleipedro <ielseu@hotmail.com>
2023-08-22 22:46:04 -0300

    config project

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express';
+import usuarioRoute from './routes/usuarioRoute'
 
 export const app = express();
+
+app.use(express.json());
+
+app.use('/usuario', usuarioRoute);
